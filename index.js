2334106edfb068f207d227fed72f5b2b4a90e8b7
陈磊 <chenlei@chenleideMacBook-Air.local>
2019-08-16 23:21:35 +0800

    棋盘

---
+++ index.js
@@ -0,0 +1,11 @@
+var chess=document.getElementById('myCanvas');
+var context=chess.getContext('2d');
+
+for(var i=0;i<15;i++){
+    context.moveTo(15+i*30,15);
+    context.lineTo(15+i*30,435);
+    context.stroke();
+    context.moveTo(15,15+i*30);
+    context.lineTo(435,15+i*30);
+    context.stroke();
+}
